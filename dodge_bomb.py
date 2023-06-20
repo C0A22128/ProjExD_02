9784e964f75c8293ee39a549b805b66d8b317984
hoshino <c0a22128b9@edu.teu.ac.jp>
2023-06-20 14:20:58 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -19,6 +19,8 @@ def main():
     y = random.randint(0, HEIGHT)
     bd_rct = bd_img.get_rect()  #爆弾Surface
     bd_rct.center = x, y  #爆弾rctの中心座標を乱数で表示
+    vx, vy = +5, +5  #練習2
+
 
     clock = pg.time.Clock()
     tmr = 0
@@ -31,10 +33,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
